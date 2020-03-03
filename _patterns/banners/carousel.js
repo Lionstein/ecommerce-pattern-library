13f09c941909f84cff93af1477e13b5bdb46c74e
Lionstein <knorquay00@gmail.com>
2020-03-03 16:30:20 -0500

    Add js carousel animation

---
+++ _patterns/banners/carousel.js
@@ -0,0 +1,10 @@
+setInterval(()=>{
+  const current = $('.carousel-input:checked');
+  const next = current.next('.carousel-input');
+  current.removeAttr('checked');
+  if (next.length) {
+    next.attr('checked', true);
+  } else {
+    $('.carousel .carousel-input:first-child').attr('checked', true);
+  }
+}, 2000);
